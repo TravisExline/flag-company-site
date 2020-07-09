f91fe11c8e5fd059b4c5a095f1298d8742ed17c8
TravisExline <t.exline22@gmail.com>
2020-07-09 09:27:15 -0500

    resized picture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style/App.css';
-import BackgroundImage from './images/flag-company-name.jpg'
+import BackgroundImage from './images/flag-company-name-2.jpg'
 import LandingPage from './containers/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <div className="company-name-container">
-          <img className="background-image" src={BackgroundImage} alt="background flag" height="200" width="1400px"></img>
+          <img className="background-image" src={BackgroundImage} alt="background flag" height="300" width="1400px"></img>
       </div>
       <LandingPage />
     </div>
