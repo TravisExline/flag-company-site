83b0678333b0947a12ad94aabf2ad56419b518e2
TravisExline <t.exline22@gmail.com>
2020-07-10 10:06:21 -0500

    flagspage/flagscard build

--- src/components/FlagCard.js
+++ src/components/FlagCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 class FlagCard extends React.Component {
     render() {
         return (
-            <div>
-                <h1>I'm the FlagCard</h1>
+            <div className="flag-photo-holder">
+                <img src={this.props.flag} className="flag-card-photo"></img>
             </div>
         )
     }
--- src/containers/FlagsPage.js
+++ src/containers/FlagsPage.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import FlagCard from '../components/FlagCard'
+import display1 from '../images/display1.jpg'
+import display2 from '../images/display2.jpg'
+import display3 from '../images/display3.jpg'
+import display4 from '../images/display4.jpg'
+import display5 from '../images/display5.jpg'
+import display6 from '../images/display6.jpg'
+import display7 from '../images/display7.jpg'
+import display8 from '../images/display8.jpg'
+import display9 from '../images/display9.jpg'
 
 class FlagsPage extends React.Component {
     render() {
+        const allFlags = [display1, display2, display3, display4, display5, display6, display7, display8, display9]
         return (
             <div>
-                <FlagCard />
+                <div className="flag-card-container">
+                    {allFlags.map((flag) => (
+                        <FlagCard flag={flag} />
+                    ))}
+                </div>
             </div>
         )
     }
