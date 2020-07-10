a5a4464251063fd31d77ee5020efa312b2e82303
TravisExline <t.exline22@gmail.com>
2020-07-10 11:27:14 -0500

    FlagsPage styling

--- src/containers/FlagsPage.js
+++ src/containers/FlagsPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import FlagCard from '../components/FlagCard'
+import '../style/FlagsPage.css'
 import display1 from '../images/display1.jpg'
 import display2 from '../images/display2.jpg'
 import display3 from '../images/display3.jpg'
@@ -12,14 +12,45 @@ import display9 from '../images/display9.jpg'
 
 class FlagsPage extends React.Component {
     render() {
-        const allFlags = [display1, display2, display3, display4, display5, display6, display7, display8, display9]
         return (
-            <div>
-                <div className="flag-card-container">
-                    {allFlags.map((flag) => (
-                        <FlagCard flag={flag} />
-                    ))}
+            <div className='all-flags-holder'>
+
+                <div className="flag-1">
+                    <img src={display1} className='flag-page-photos' alt='flag'></img>
                 </div>
+
+                <div className="flag-2">
+                    <img src={display2} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-3">
+                    <img src={display3} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-4">
+                    <img src={display4} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-5">
+                    <img src={display5} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-6">
+                    <img src={display6} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-7">
+                    <img src={display7} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-8">
+                    <img src={display8} className='flag-page-photos' alt='flag'></img>
+                </div>
+
+                <div className="flag-9">
+                    <img src={display9} className='flag-page-photos' alt='flag'></img>
+                </div>
+
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import FlagsPage from './containers/FlagsPage'
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
+      <Router>
         <div>
-          <App />
-        </div>,
+          <Route exact path='/' component={App} />
+          <Route exact path='/flags' component={FlagsPage} />
+        </div>
+      </Router>,
 
 document.getElementById('root')
 );
