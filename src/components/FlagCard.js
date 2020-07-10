419ee3a18fefd90190df13ac51db000e25a264c2
TravisExline <t.exline22@gmail.com>
2020-07-10 10:00:41 -0500

    setting up FlagsPage/FlagCard

---
+++ src/components/FlagCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class FlagCard extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I'm the FlagCard</h1>
+            </div>
+        )
+    }
+}
+
+export default FlagCard
---
+++ src/containers/FlagsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FlagCard from '../components/FlagCard'
+
+class FlagsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <FlagCard />
+            </div>
+        )
+    }
+}
+
+export default FlagsPage
