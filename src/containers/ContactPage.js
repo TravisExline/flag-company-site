fcb6b55f823ba59d2019f1bbe72fd1ca78d8f5d9
TravisExline <t.exline22@gmail.com>
2020-07-16 10:01:19 -0500

    contact

--- src/containers/ContactPage.js
+++ src/containers/ContactPage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import '../style/Contact.css'
 
 class ContactPage extends React.Component {
     render() {
         return (
             <div>
                 <NavBar />
-                <p>Contact Page</p>
+                <div className="contact-holder">
+                    <div className="contact-details">
+                        <h2 id="email">Email: texline5@gmail.com</h2>
+                        <h2 id="facebook">Visit Our <a href="https://www.facebook.com/Scorched-Wood-Flag-Co-103691608050454">Facebook</a></h2>
+                    </div>
+                </div>
             </div>
         )
     }
