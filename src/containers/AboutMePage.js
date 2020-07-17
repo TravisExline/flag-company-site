4a9a557da036903a7317edf57e4330128147827d
TravisExline <t.exline22@gmail.com>
2020-07-17 08:15:27 -0500

    working about me

--- src/containers/AboutMePage.js
+++ src/containers/AboutMePage.js
@@ -11,6 +11,10 @@ class AboutMePage extends React.Component {
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+                <h2 className="team-heading">The Team</h2>
+                <h3 className="founder-craftsman">Founder and Craftsman</h3>
+                <img src='' alt="Founder and Craftsman Photo"></img>
+                <p className="founder-craftsman-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
         )
     }
