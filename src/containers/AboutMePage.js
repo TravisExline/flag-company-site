a33025097a6146d66ec7bb6288552abc2dae2f25
TravisExline <t.exline22@gmail.com>
2020-07-21 11:14:26 -0500

    heroku test fix

--- src/containers/AboutMePage.js
+++ src/containers/AboutMePage.js
@@ -12,7 +12,7 @@ class AboutMePage extends React.Component {
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <img src="" alt="Workshop"></img>
+                <img src="" alt="Workshop" className="workshop-photo"></img>
                 <h2 className="team-heading">The Team</h2>
                 <h3 className="founder-craftsman">Founder and Craftsman -  Todd Exline</h3>
                 <div className="founder-photo-holder">
