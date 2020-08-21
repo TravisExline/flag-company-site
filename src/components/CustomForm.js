1e1a7c67271f9cf74986b7d35956f38829216666
TravisExline <t.exline22@gmail.com>
2020-08-21 08:35:00 -0500

    rough form done

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -3,8 +3,12 @@ import React from 'react'
 class CustomForm extends React.Component {
     render() {
         return (
-            <div>
-                Hello!
+            <div className='custom-form-holder'>
+                <form className='custom-flag-form'>
+                    <input className='design-description' type='text' placeholder='Describe Your Design' />
+                    <input className='design-photo' type='img' placeholder='Upload an Image of Your Design' />
+                    <input className='submit-flag' type='submit' value='Request This Design' />
+                </form>
             </div>
         )
     }
