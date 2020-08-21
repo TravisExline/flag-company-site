dcb5811b8880db66a3d70ccb1fb0792ccc9ee83e
TravisExline <t.exline22@gmail.com>
2020-08-21 08:50:11 -0500

    custom flag form working

--- src/actions/flagActions.js
+++ src/actions/flagActions.js
@@ -1,6 +1,7 @@
 export const ADD_FLAG = "ADD_FLAG"
 export const FETCH_FLAG = "FETCH_FLAG"
 export const SET_FLAGS= "SET_FLAGS"
+export const CREATE_FLAG = "CREATE_FLAG"
 
 export const addFlag = flag => {
     return {type: ADD_FLAG, flag}
@@ -17,4 +18,20 @@ export const fetchFlags = () => {
     .then(data => {
         dispatch(setFlags(data))
     })
+}
+
+export const createFlag = flag => {
+    return dispatch => 
+        fetch("http://localhost:3001/flags", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({flag})
+        })
+        .then(res => res.json())
+        .then(flag => {
+            dispatch(addFlag(flag))
+        })
 }
--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import {createFlag} from '../actions/flagActions'
+import {connect} from 'react-redux'
 
 class CustomForm extends React.Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.createFlag({...this.state})
+        this.setState({
+            customDesign: "",
+            customImg: "",
+            customerEmail: ""
+        })
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div className='custom-form-holder'>
-                <form className='custom-flag-form'>
-                    <input className='design-description' type='text' placeholder='Describe Your Design' />
-                    <input className='design-photo' type='img' placeholder='Upload an Image of Your Design' />
+                <form className='custom-flag-form' onSubmit={event => this.handleSubmit(event)}>
+                    <input className='design-description' type='text' name='customDesign' placeholder='Describe Your Design' onChange={event => this.handleChange(event)} value={this.props.customDesign}/>
+                    <input className='design-photo' type='img' name='customImg' placeholder='Upload an Image of Your Design' onChange={event => this.handleChange(event)} value={this.props.customImg}/>
+                    <input className='customer-email' type='text' name='customerEmail' placeholder='Enter Your Email' onChange={event => this.handleChange(event)} value={this.props.customerEmail} />
                     <input className='submit-flag' type='submit' value='Request This Design' />
                 </form>
             </div>
@@ -14,4 +34,4 @@ class CustomForm extends React.Component {
     }
 }
 
-export default CustomForm
+export default connect(null, {createFlag})(CustomForm)
