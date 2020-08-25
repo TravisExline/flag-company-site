8a8d6612b26e6c35dafbfc898c2d235b4a9f17eb
TravisExline <t.exline22@gmail.com>
2020-08-25 12:07:14 -0500

    date working!

--- src/components/TestCalendar.js
+++ src/components/TestCalendar.js
@@ -11,9 +11,27 @@ class TestCalendar extends React.Component {
         }
     }
 
-    showDate() {
-        this.props.myEvents.map((myEvent) => {
-            if (myEvent.eventDate == this.state.todaysDate.toString) {
+    // showDate() {
+    //     this.props.myEvents.map((myEvent) => {
+    //         if (myEvent.eventDate == this.state.todaysDate) {
+    //             console.log("you win!")
+    //             return (
+    //             <div>
+    //                 <h3>{myEvent.eventDate}</h3>
+    //                 <h3>{myEvent.eventStart} - {myEvent.eventEnd}</h3>
+    //                 <h2>{myEvent.eventName}</h2>
+    //             </div> )
+    //         } else {
+    //             console.log('you suck')
+    //         }
+    //     })
+    // }
+
+    render() {
+        // debugger
+        const correctDate = this.props.myEvents.map((myEvent) => {
+            if (myEvent.eventDate == this.state.todaysDate) {
+                console.log("you win!")
                 return (
                 <div>
                     <h3>{myEvent.eventDate}</h3>
@@ -22,13 +40,9 @@ class TestCalendar extends React.Component {
                 </div> )
             }
         })
-    }
-
-    render() {
-        debugger
         return(
             <div>
-                {this.showDate()}
+                {correctDate}
             </div>
         )
     }
