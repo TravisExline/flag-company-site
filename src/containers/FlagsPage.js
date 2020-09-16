85184a65def1d8125bd67744b571e11b1a159db0
TravisExline <t.exline22@gmail.com>
2020-09-16 07:49:42 -0500

    working on flags page

--- src/containers/FlagsPage.js
+++ src/containers/FlagsPage.js
@@ -4,6 +4,8 @@ import FlagCard from '../components/FlagCard'
 import NavBar from '../components/NavBar'
 import { fetchFlags } from '../actions/flagActions'
 import { connect } from 'react-redux'
+import CustomFormHolder from './CustomFormHolder'
+import MyCalendar from './MyCalendar'
 
 class FlagsPage extends React.Component {
 
@@ -12,24 +14,34 @@ class FlagsPage extends React.Component {
     }
 
     render() {
-        return (
+        return(
             <div>
                 <NavBar />
-            <div className="order">
-                <h2>To Order, Please Email Us With Your Desired Size and Design</h2>
-            </div>
-            <div className='all-flags-holder'>
-                {this.props.flagDisplay.flags.map((flag) => (
-                    <FlagCard flag={flag.photo} name={flag.name} smallPrice={flag.smallPrice} medPrice={flag.medPrice} lrgPrice={flag.lrgPrice}/>
-                ))}
-                {this.props.newFlag.map((flag) => (
-                    <p>{flag.name}{flag.id}</p>
-                ))
-                }
-            </div>
+                <CustomFormHolder />
+                <MyCalendar />
             </div>
         )
     }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <NavBar />
+    //         <div className="order">
+    //             <h2>To Order, Please Email Us With Your Desired Size and Design</h2>
+    //         </div>
+    //         <div className='all-flags-holder'>
+    //             {this.props.flagDisplay.flags.map((flag) => (
+    //                 <FlagCard flag={flag.photo} name={flag.name} smallPrice={flag.smallPrice} medPrice={flag.medPrice} lrgPrice={flag.lrgPrice}/>
+    //             ))}
+    //             {this.props.newFlag.map((flag) => (
+    //                 <p>{flag.name}{flag.id}</p>
+    //             ))
+    //             }
+    //         </div>
+    //         </div>
+    //     )
+    // }
 }
 
 const mapStateToProps = (state) => {
