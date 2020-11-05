a5c0b4118cbe584739977dbd4515a3f979a9f066
TravisExline <t.exline22@gmail.com>
2020-11-05 08:01:49 -0600

    working on cutom form

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import NavBar from './NavBar'
+import '../style/CustomForm.css'
 import {createFlag} from '../actions/flagActions'
 import {connect} from 'react-redux'
 
@@ -22,13 +24,19 @@ class CustomForm extends React.Component {
 
     render() {
         return (
-            <div className='custom-form-holder'>
-                <form className='custom-flag-form' onSubmit={event => this.handleSubmit(event)}>
-                    <input className='design-description' type='text' name='customDesign' placeholder='Describe Your Design' onChange={event => this.handleChange(event)} value={this.props.customDesign}/>
-                    <input className='design-photo' type='img' name='customImg' placeholder='Upload an Image of Your Design' onChange={event => this.handleChange(event)} value={this.props.customImg}/>
-                    <input className='customer-email' type='text' name='customerEmail' placeholder='Enter Your Email' onChange={event => this.handleChange(event)} value={this.props.customerEmail} />
-                    <input className='submit-flag' type='submit' value='Request This Design' />
-                </form>
+            <div>
+                <div className="custom-header">
+                    <h1 id="custom-company-name">Scorched Wood Flag Company</h1>
+                    <div className='nav-holder'><NavBar/></div>
+                </div>
+                <div className='custom-form-holder'>
+                    <form className='custom-flag-form' onSubmit={event => this.handleSubmit(event)}>
+                        <input className='design-description' type='text' name='customDesign' placeholder='Describe Your Design' onChange={event => this.handleChange(event)} value={this.props.customDesign}/>
+                        <input className='design-photo' type='img' name='customImg' placeholder='Upload an Image of Your Design' onChange={event => this.handleChange(event)} value={this.props.customImg}/>
+                        <input className='customer-email' type='text' name='customerEmail' placeholder='Enter Your Email' onChange={event => this.handleChange(event)} value={this.props.customerEmail} />
+                        <input className='submit-flag' type='submit' value='Request This Design' />
+                    </form>
+                </div>
             </div>
         )
     }
